805da6f16a1e820915b654af4a564c839614236e
kimkyubae <kkyub5118@gmail.com>
2022-03-03 17:34:13 +0900

    곰세마리 연주

--- chapter4/build/sketch/app.ino.cpp
+++ chapter4/build/sketch/app.ino.cpp
@@ -1,24 +1,53 @@
 #include <Arduino.h>
-#line 1 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex01/app.ino"
-// 부저 울리기(능동 부저)
-const int buzzer_pin = 9;
+#line 1 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex02/app.ino"
+#define NOTE_C4 262 // 4옥타브 도
+#define NOTE_D4 294 // 4옥타브 레
+#define NOTE_E4 330 // 4옥타브 미
+#define NOTE_G4 392 // 4옥타브 솔
+#define NOTE_A4 440 // 4옥타브 라
+#define NOTE_C5 523 // 5옥타브 레
+#define NUM 49
 
+int melody[NUM] = { // 곰 세마리
+    // 도도도도도 // 미솔솔미도
+    NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4,          //도도도도도
+    NOTE_E4, NOTE_G4, NOTE_G4, NOTE_E4, NOTE_C4,          //미솔솔미도
+    NOTE_G4, NOTE_G4, NOTE_E4, NOTE_G4, NOTE_G4, NOTE_E4, //솔솔미솔솔미
+    NOTE_C4, NOTE_C4, NOTE_C4,                            //도도도
 
-#line 5 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex01/app.ino"
+    NOTE_G4, NOTE_G4, NOTE_E4, NOTE_C4,          //솔솔 미도
+    NOTE_G4, NOTE_G4, NOTE_G4,                   //솔솔솔
+    NOTE_G4, NOTE_G4, NOTE_E4, NOTE_C4,          //솔솔 미도
+    NOTE_G4, NOTE_G4, NOTE_G4,                   //솔솔솔
+    NOTE_G4, NOTE_G4, NOTE_E4, NOTE_C4,          // 솔솔미도
+    NOTE_G4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, // 솔솔솔라솔
+    NOTE_C5, NOTE_G4, NOTE_C5, NOTE_G4,          //도솔도솔
+    NOTE_E4, NOTE_D4, NOTE_C4};                  // 미레도
+
+// 음표 길이
+int noteDuration[NUM] = {
+    4, 8, 8, 4, 4, 4, 8, 8, 4, 4, 8, 8, 4, 8, 8, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2,
+    4, 4, 4, 4, 8, 8, 8, 8, 2, 4, 4, 4, 4, 4, 4, 2};
+const int speaker_pin = 9;
+#line 30 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex02/app.ino"
 void setup();
-#line 10 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex01/app.ino"
+#line 35 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex02/app.ino"
 void loop();
-#line 5 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex01/app.ino"
+#line 30 "/Users/qbae/Workspace/Arduino/chapter4/buzzer/ex02/app.ino"
 void setup()
 {
-    pinMode(buzzer_pin, OUTPUT);
+    pinMode(speaker_pin, OUTPUT);
 }
 
 void loop()
 {
-    digitalWrite(buzzer_pin, HIGH);
-    delay(200);
-
-    digitalWrite(buzzer_pin, LOW);
-    delay(100);
+    int m, d, dd;
+    for (m = 0; m < NUM; m++)
+    {
+        d = 1000 / noteDuration[m];
+        dd = d * 1.3; // 음 출력 시간(4분 음표 325ms)
+        tone(speaker_pin, melody[m], d);
+        delay(dd);
+    }
+    delay(1000);
 }
